bc726bba2af4a8fd0483a6892233149a3009dede
Lars Vilhuber <lars.vilhuber@cornell.edu>
2019-10-29 19:11:47 +0000

    Cleaning up, with better inclusions and exclusions.

--- programs/02_subset_onet.R
+++ programs/02_subset_onet.R
@@ -4,21 +4,20 @@
 source(file.path(rprojroot::find_rstudio_root_file(),"pathconfig.R"),echo=FALSE)
 source(file.path(programs,"config.R"), echo=FALSE)
 
-# load onet data
+# ========= load onet data ============
 Alternate_Titles <- read_excel(file.path(acquired,"Alternate Titles.xlsx"))
 # merge in verbose job titles
 Occupation_Data <- read_excel(file.path(acquired,"Occupation Data.xlsx")) %>% select(-Title)
 
 # Possible merge in the Transition matrix, as a potential for alternate hires
 
-# Read BLS earnings data
+# ============ Read BLS earnings data ===============
 BLS.data <- read_excel(file.path(acquired,paste0("national_M20",oes.src.version,"_dl.xlsx")))
 
-# load the jobtitle data (our own list)
-job_titles <- read_excel(file.path(generated,"job_titles.xlsx"),sheet = "Main")
-names(job_titles) <- c("Job Title")
+# ============ load the jobtitle data (our own list) ==================
+job_titles.main <- read_excel(file.path(generated,"job_titles.xlsx"),sheet = "Main",col_names = TRUE)
 
-# we might want to add a few more to improve match probabilities
+# we might want to add a few more to improve match probabilities. These augment the "official list"
 job_titles.plus <- read_excel(file.path(generated,"job_titles.xlsx"),
                               sheet="Inclusions",col_names = TRUE) %>%
   rename("Job Title" = "Title") %>%  
@@ -26,28 +25,31 @@ job_titles.plus <- read_excel(file.path(generated,"job_titles.xlsx"),
   unite("O*NET-SOC Code",  "SOC",tmp,sep = ".",remove = FALSE) %>% 
   select(-tmp)
 
+job_titles <- bind_rows(job_titles.main,
+                        job_titles.plus %>% select("Alternate Title") 
+                           %>% rename("Job Title" = "Alternate Title")) %>%
+              distinct()
+
 job_titles.minus <- read_excel(file.path(generated,"job_titles.xlsx"),
                                sheet="Exclusions",col_names = TRUE) %>%
   rename("Job Title" = "Title") 
 
-# get both the official 
+# =========== Processing ======================
+# get both the official and the alternate
 soc_job_titles <- Alternate_Titles %>% select("O*NET-SOC Code",Title) %>%
   distinct() 
 soc_job_alttitles <- Alternate_Titles %>% select("O*NET-SOC Code","Alternate Title") %>%
   distinct() 
-soc_job_alttitles <- bind_rows(soc_job_alttitles,job_titles.plus %>%
-                                 select("Alternate Title","O*NET-SOC Code")
-                              )
 
 primary <- stringdist_inner_join(y=soc_job_titles,
-                                 x=job_titles,
+                                 x=job_titles %>% select("Job Title"),
                                  by = c("Job Title" = "Title"),
                                  method="jw",
                                  distance_col="jw_distance",
                                  max_dist=0.05) %>% 
             mutate(Alternate = "0")
 secondary.raw <- stringdist_inner_join(y=soc_job_alttitles,
-                                   x=job_titles,
+                                   x=job_titles %>% select("Job Title"),
                                    by = c("Job Title" = "Alternate Title"),
                                    method="jw",
                                    distance_col="jw_distance",
@@ -56,10 +58,7 @@ secondary.raw <- stringdist_inner_join(y=soc_job_alttitles,
               mutate(Alternate = "1") 
 
 # get back the augmented job titles
-secondary <- secondary.raw %>%  left_join(job_titles.plus %>% 
-  select(-"O*NET-SOC Code"),by=c("Alternate Title")) %>% 
-  mutate(`Job Title` = if_else(is.na(SOC),`Job Title.x`,`Job Title.y`)) %>% 
-  select(-`Job Title.x`,-`Job Title.y`,-SOC) 
+secondary <- secondary.raw 
 
 
 # Still needs frequencies of relation alternate to primary
@@ -67,11 +66,16 @@ nlm.titles.raw <- bind_rows(primary,secondary) %>%
   left_join(Occupation_Data,by="O*NET-SOC Code") %>%
   separate("O*NET-SOC Code",sep = 7,remove=FALSE,into = c("SOC","suffix")) %>%
   select(-suffix) %>%
-  left_join(BLS.data,by=c("SOC" = "OCC_CODE")) %>%
-  right_join(job_titles,by="Job Title")
+  left_join(BLS.data,by=c("SOC" = "OCC_CODE"))
+
+# We have a few in there that are our alternates
+nlm.titles.raw2 <-  nlm.titles.raw %>%
+  left_join(job_titles.plus %>% select(-`O*NET-SOC Code`,-SOC),by=c("Job Title" = "Alternate Title")) %>%
+  mutate("Job Title" = if_else(is.na(`Job Title.y`),`Job Title`,`Job Title.y`)) %>%
+  right_join(job_titles.main,by="Job Title")
 
 # Filter out the ones we know to be wrong
-nlm.titles <- anti_join(nlm.titles.raw,job_titles.minus %>% select(SOC))
+nlm.titles <- anti_join(nlm.titles.raw2,job_titles.minus %>% select(SOC),by="SOC")
 
 
 saveRDS(nlm.titles,file = file.path(outputs,"nlm.titles.RDS"))
